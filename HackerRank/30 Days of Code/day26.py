6ae90018570ca7d0a2e98a475a4082dfc4c0b72e
InnO <63262620+mkp042@users.noreply.github.com>
2020-07-12 23:56:43 +0530

    update something

--- HackerRank/30 Days of Code/day26.py
+++ HackerRank/30 Days of Code/day26.py
@@ -1,23 +1,14 @@
-# GEEKFORGEEK
+d1 = list(map(int, input().split()))
+d2 = list(map(int, input().split()))
 
-# Example:
-# Input:
-# 2
-# i.like.this.program.very.much
-# pqr.mno
+fine = 0
+if d1[2] > d2[2]:
+    fine = 10000
+elif d1[2] == d2[2]:
+    if d1[1] > d2[1]:
+        fine = 500 * (d1[1] - d2[1])
+    elif d1[1] == d2[1]:
+        if d1[0] > d2[0]:
+            fine = 15 * (d1[0] - d2[0])
 
-# Output:
-# much.very.program.this.like.i
-# mno.pqr
-
-
-# from collections import deque
-
-for _ in range(int(input())):
-
-    var1 = input().split(".")
-
-    var1.reverse()
-
-    var1 = ".".join(var1)
-    print(var1)
+print(fine)
