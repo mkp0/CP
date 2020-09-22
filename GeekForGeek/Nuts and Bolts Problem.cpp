8ab21d8760ceb0305bd2080c7e561ec4aa045339
InnO <63262620+mkp042@users.noreply.github.com>
2020-09-22 18:40:22 +0530

    gfg

---
+++ GeekForGeek/Nuts and Bolts Problem.cpp
@@ -0,0 +1,10 @@
+//User function template for C++
+class Solution
+{
+public:
+    void matchPairs(char nuts[], char bolts[], int n)
+    {
+        sort(nuts, nuts + n);
+        sort(bolts, bolts + n);
+    }
+};
